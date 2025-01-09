da7c63ce04fb5291e9ceb42b94070bf7d4856ea7
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-01-09 14:22:35 -0500

    Collapsing forall and exists in smt query

--- backend/propencoding.ml
+++ backend/propencoding.ml
@@ -2,6 +2,20 @@ open Z3
 open Z3aux
 open Language
 
+let rec collapse_forall prop =
+  match prop with
+  | Forall { qv; body } ->
+      let vars, body = collapse_forall body in
+      (qv :: vars, body)
+  | _ -> ([], prop)
+
+let rec collapse_exist prop =
+  match prop with
+  | Exists { qv; body } ->
+      let vars, body = collapse_exist body in
+      (qv :: vars, body)
+  | _ -> ([], prop)
+
 let to_z3 ctx prop =
   let rec aux prop =
     match prop with
@@ -11,10 +25,18 @@ let to_z3 ctx prop =
     | And ps -> Boolean.mk_and ctx (List.map aux ps)
     | Or ps -> Boolean.mk_or ctx (List.map aux ps)
     | Iff (p1, p2) -> Boolean.mk_iff ctx (aux p1) (aux p2)
-    | Forall { qv; body } ->
-        make_forall ctx [ tpedvar_to_z3 ctx (qv.ty, qv.x) ] (aux body)
-    | Exists { qv; body } ->
-        make_exists ctx [ tpedvar_to_z3 ctx (qv.ty, qv.x) ] (aux body)
+    | Forall _ ->
+        let qvs, body = collapse_forall prop in
+        let qv_expr_list =
+          List.map (fun { x; ty } -> tpedvar_to_z3 ctx (ty, x)) qvs
+        in
+        make_forall ctx qv_expr_list (aux body)
+    | Exists _ ->
+        let qvs, body = collapse_exist prop in
+        let qv_expr_list =
+          List.map (fun { x; ty } -> tpedvar_to_z3 ctx (ty, x)) qvs
+        in
+        make_exists ctx qv_expr_list (aux body)
     | Lit lit -> Litencoding.typed_lit_to_z3 ctx lit
   in
   aux prop
