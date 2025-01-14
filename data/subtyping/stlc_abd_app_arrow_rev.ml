f36dec4c8d0be51a94781cebdb56a990cca44c6f
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-01-14 15:17:14 -0500

    Flip order for consistency

--- data/subtyping/stlc_abd_app_arrow_rev.ml
+++ data/subtyping/stlc_abd_app_arrow_rev.ml
@@ -1,14 +1,3 @@
-let[@assert] rty2 =
-  let num_arr_tau = (v >= 0 : [%v: int]) [@over] in
-  let num = (v >= 0 : [%v: int]) [@over] in
-  let gamma = (true : [%v: stlc_tyctx]) [@over] in
-  let tau = (num_arr v num_arr_tau : [%v: stlc_ty]) [@over] in
-  (((is_abs v && (not (is_app v)) && (not (num == 0)) && not (num_arr_tau == 0))
-   || (is_abs v && (not (is_app v)) && (not (num == 0)) && num_arr_tau == 0))
-   && typing gamma v tau && num_app v num
-    : [%v: stlc_term])
-    [@under]
-
 let[@assert] rty1 =
   let num_arr_tau = (v >= 0 : [%v: int]) [@over] in
   let num = (v >= 0 : [%v: int]) [@over] in
@@ -19,5 +8,16 @@ let[@assert] rty1 =
    && num_app v num
    && fun ((body [@exists]) : stlc_term) ->
    stlc_abs_ty v tau1 && stlc_abs_body v body
+    : [%v: stlc_term])
+    [@under]
+
+let[@assert] rty2 =
+  let num_arr_tau = (v >= 0 : [%v: int]) [@over] in
+  let num = (v >= 0 : [%v: int]) [@over] in
+  let gamma = (true : [%v: stlc_tyctx]) [@over] in
+  let tau = (num_arr v num_arr_tau : [%v: stlc_ty]) [@over] in
+  (((is_abs v && (not (is_app v)) && (not (num == 0)) && not (num_arr_tau == 0))
+   || (is_abs v && (not (is_app v)) && (not (num == 0)) && num_arr_tau == 0))
+   && typing gamma v tau && num_app v num
     : [%v: stlc_term])
     [@under]
