190c85e08b790f79c012ca84c82fc62516d5e84d
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-01-15 14:29:47 -0500

    Try to handle partial application of operations and a more restrictive rec_arg2 recursion

--- data/predefined/templates.ml
+++ data/predefined/templates.ml
@@ -1,7 +1,8 @@
 let[@axiom] rec_arg (arg : int) (param : int) = param >= 0 && param < arg
 
 let[@axiom] rec_arg2 (arg1 : int) (param1 : int) (arg2 : int) (param2 : int) =
-  param1 >= 0 && param2 >= 0 && (param1 < arg1 || param2 < arg2)
+  param1 >= 0 && param2 >= 0
+  && ((param1 < arg1 && param2 == arg2) || param2 < arg2)
 
 let[@axiom] template_eq_0 (x : int) = x == 0
 let[@axiom] template_lt (a : int) (b : int) = a < b
