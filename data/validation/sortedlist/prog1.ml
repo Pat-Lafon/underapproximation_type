38b9ac864b8640664d1950b67d870c22324e488b
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-02-05 13:04:29 -0500

    sortedlist axioms

--- data/validation/sortedlist/prog1.ml
+++ data/validation/sortedlist/prog1.ml
@@ -10,8 +10,9 @@ let rec sorted_list_gen (s : int) (x : int) : int list =
     else Exn
 
 let[@assert] sorted_list_gen =
-  let s = (0 <= v : [%v: int]) [@over] in
-  let x = (true : [%v: int]) [@over] in
-  (len v s && sorted v && fun (u : int) -> (hd v u) #==> (x <= u)
+  let s = ((0 <= v : [%v: int]) [@over]) in
+  let x = ((true : [%v: int]) [@over]) in
+  ((len v s && sorted v
+    && (not (emp v)) #==> (fun ((u [@exists]) : int) -> hd v u && x <= u)
     : [%v: int list])
-    [@under]
+    [@under])
