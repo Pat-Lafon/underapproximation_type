818fcff5e0a0f2c394859e07fd17267370f67be3
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-02-10 11:16:56 -0500

    Complete tree results

--- data/validation/complete_tree/prog.ml
+++ data/validation/complete_tree/prog.ml
@@ -8,5 +8,5 @@ let rec complete_tree_gen (s : int) : int tree =
     Node (n, lt, rt)
 
 let[@assert] complete_tree_gen =
-  let s = (0 <= v : [%v: int]) [@over] in
-  (depth v s && complete v : [%v: int tree]) [@under]
+  let s = ((0 <= v : [%v: int]) [@over]) in
+  ((depth v s && complete v : [%v: int tree]) [@under])
