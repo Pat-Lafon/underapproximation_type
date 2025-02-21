0420d44663a28d4a3ab7c1b02e8d0ab6b6e163bc
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-02-21 14:27:55 -0500

    Latest small fix and rerun all the numbers

--- backend/smtquery.ml
+++ backend/smtquery.ml
@@ -17,7 +17,7 @@ let ctx =
       (* ("rlimit", "80000000"); (* 21 timeouts*) *)
       (* ("rlimit", "120000000"); (* 16ish timeouts *) *)
       (* ("rlimit", "250000000"); (* 12ish I think timeouts *) *)
-      ("rlimit", "80000000");
+      ("rlimit", "8000000");
       (* 13 timeouts *)
     ]
 
