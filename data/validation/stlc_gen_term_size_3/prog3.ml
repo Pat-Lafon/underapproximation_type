9685d4eed6876d5cc584d7d1ca9ee712cfa0201f
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-02-25 11:40:33 -0500

    Current stlc numbers

--- data/validation/stlc_gen_term_size_3/prog3.ml
+++ data/validation/stlc_gen_term_size_3/prog3.ml
@@ -24,10 +24,23 @@ let rec gen_term_size (num_arr_tau : int) (num : int) (gamma : stlc_tyctx)
       Stlc_app (func, arg)
     else Err
   else match tau with Stlc_ty_nat -> Err | Stlc_ty_arr (tau1, tau2) -> Err
+(* let (idx10 : stlc_ty) = tau2 in
+        let (idx60_0 : int) = get_num_arr idx10 in
+        let (idx570 : int -> stlc_tyctx -> stlc_ty -> stlc_term) =
+          gen_term_size idx60_0
+        in
+        let (idx1 : int) = num in
+        let (idx571 : stlc_tyctx -> stlc_ty -> stlc_term) = idx570 idx1 in
+        let (idx2 : stlc_tyctx) = gamma in
+        let (idx9 : stlc_ty) = tau1 in
+        let (idx62_20 : stlc_tyctx) = Stlc_tyctx_cons (idx9, idx2) in
+        let (idx572 : stlc_ty -> stlc_term) = idx571 idx62_20 in
+        let (idx573_1 : stlc_term) = idx572 idx10 in
+        Stlc_abs (idx9, idx573_1) *)
 
 let[@assert] gen_term_size =
-  let num_arr_tau = (v >= 0 : [%v: int]) [@over] in
-  let num = (v >= 0 : [%v: int]) [@over] in
-  let gamma = (true : [%v: stlc_tyctx]) [@over] in
-  let tau = (num_arr v num_arr_tau : [%v: stlc_ty]) [@over] in
-  (typing gamma v tau && num_app v num : [%v: stlc_term]) [@under]
+  let num_arr_tau = ((v >= 0 : [%v: int]) [@over]) in
+  let num = ((v >= 0 : [%v: int]) [@over]) in
+  let gamma = ((true : [%v: stlc_tyctx]) [@over]) in
+  let tau = ((num_arr v num_arr_tau : [%v: stlc_ty]) [@over]) in
+  ((typing gamma v tau && num_app v num : [%v: stlc_term]) [@under])
