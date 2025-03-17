f21fb5457498797a96b42da3e506bc4a06017e28
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-03-17 10:07:14 -0400

    stuff for new sketches

--- data/validation/depth_bst/prog5.ml
+++ data/validation/depth_bst/prog5.ml
@@ -1,13 +1,9 @@
 let rec size_bst_gen (d : int) (lo : int) (hi : int) : int tree =
-  if sizecheck d then Leaf
+  if sizecheck d then Err
   else if incr lo < hi then
     let (x : int) = int_range lo hi in
-    if bool_gen () then
-      let (lt : int tree) = size_bst_gen (subs d) lo x in
-      let (rt : int tree) = size_bst_gen (subs d) x hi in
-      Node (x, lt, rt)
-    else Leaf
-  else Leaf
+    if bool_gen () then Err else Err
+  else Err
 
 let[@assert] size_bst_gen =
   let d = ((0 <= v : [%v: int]) [@over]) in
